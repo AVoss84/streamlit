aae05e521e12474636a8bc0551be5f8795652ca8
AVoss <a.vosseler@gmx.net>
2021-10-25 20:58:43 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 from copy import deepcopy
 
-plt.rcParams["figure.figsize"] = (9,2)
+#plt.rcParams["figure.figsize"] = (9,2)
 
 # Use the full page instead of a narrow central column
 st.set_page_config(layout="wide")
@@ -27,6 +27,77 @@ def load_data_csv(file_byte_object):
 @st.cache
 def load_data_xls(file_byte_object):
     return pd.read_excel(file_byte_object)
+
+
+@st.cache
+def create_filter_string(lookup : dict, lookup_assigned : dict, verbose : bool = False) -> str:
+    query_string = ""
+    for i, cols in enumerate(lookup.keys()):
+        if len(lookup_assigned[cols]) > 0: 
+            if i==0:
+                query_string += "{} == {}".format(cols, lookup_assigned[cols])
+            else:    
+                query_string += ' & ' + "{} == {}".format(cols, lookup_assigned[cols])
+        if verbose : print(query_string)
+    return query_string 
+
+@st.cache
+def filter_data(df : pd.DataFrame, query_string : str):
+    return df.query(query_string)
+
+
+@st.cache
+def make_line_plot(df : pd.DataFrame, used_columns = ['time', 'target2', 'est2', 'lower2', 'upper2']):
+    """Create trace plot"""
+    fig = go.Figure([
+    go.Scatter(
+        x = df['time'], y = df['target2'],
+        line=dict(color='rgb(31, 119, 180)'),
+        mode='markers+lines', name = 'Observed'
+    ),
+        go.Scatter(
+        x = df['time'].tail(1), y = df['target2'].tail(1),
+        mode='markers', name = 'Predicted out-of-sample', fillcolor='rgba(68, 68, 68, 0.3)'
+    ),
+        go.Scatter(
+        x = df['time'], y = df['est2'],
+        line=dict(color='rgba(80,50,50,0.5)'),
+        mode='markers+lines', name = 'Predicted in-sample'
+    ),
+        go.Scatter(
+        x = df['time'], y = df['upper2'],
+        line=dict(color='rgba(50,50,50,0.1)', width=4, dash='dot'),
+        fill=None, name = 'prediction lower/upper bound')
+
+    , go.Scatter(
+        x = df['time'], y = df['lower2'],
+        line=dict(color='rgba(50,50,50,0.1)', width=4, dash='dot'),
+        fill='tonexty', name = 'prediction lower bound', showlegend=False
+    )
+    ])
+
+    fig.update_layout(
+        yaxis_title='target',
+        title='Target with 1-month ahead prediction', legend={'traceorder':'normal'}, 
+        hovermode="x")
+
+    fig.update_xaxes(dtick="M1", tickformat="%b\n%Y", tickangle = 90)    
+    return fig
+
+
+@st.cache
+def make_hist_plot(df : pd.DataFrame, used_columns = ['time', 'target1', 'est1', 'lower1', 'upper1']):
+    """Create histogram based on absolute frequencies"""
+    counts, bins = np.histogram(df['target2'], bins=30)
+    bins = 0.5 * (bins[:-1] + bins[1:])
+    fig = px.bar(x=bins, y=counts, labels={'x':'target', 'y':'frequency'})
+    return fig
+
+
+######################################################################################################################################################################
+################ Start app
+
+
 
 
 with header:
@@ -90,18 +161,27 @@ with dataset:
             filter_data = deepcopy(df)
         st.dataframe(filter_data)
 
+        st.markdown("***")
+
+        # Add a sidebar
+        st.subheader("Plots:")
 
         with my_expander1:
-            st.subheader('Plot 1')
-            for vals in selected_options:
-                # Hisrogram setup
-                fig, ax = plt.subplots()
-                ax.hist(df[selected_options].values, bins=30, alpha=0.5, histtype='stepfilled')
-                st.pyplot(fig)
+            #st.subheader('Plot 1')
+            #for vals in selected_options:
+            #    # Histogram setup
+            fig = make_hist_plot(df = filter_data)
+            st.plotly_chart(fig)
+        
+            #fig, ax = plt.subplots()
+            #ax.hist(filter_data['target2'].values, bins=50, alpha=0.5, histtype='stepfilled')
+            #st.pyplot(fig)
 
         with my_expander2:
-                st.subheader('Plot 2')
-                st.line_chart(df[selected_options])        
+                #st.subheader('Plot 2')
+                #st.line_chart(df[selected_options])       
+                fig = make_line_plot(df = filter_data)
+                st.plotly_chart(fig)     
             
     else:
         st.write("No dataset uploaded.")        
