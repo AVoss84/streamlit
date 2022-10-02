bd29b888312a515aa2c14defb16ea70eed87a068
Alexander Vosseler <a.vosseler@gmx.net>
2022-10-02 13:06:38 +0200

    add fastapi example

---
+++ create_api.py
@@ -0,0 +1,30 @@
+
+from unittest import result
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+from src.test_fct_fastapi import calculate
+
+#calculate(x = 2, y = 3, operation= 'Multiplication')
+
+class User_input(BaseModel):
+    operation : str
+    x: float
+    y: float
+
+
+app = FastAPI()
+
+@app.get("/health")
+async def root():
+    return {"message": "Service is healthy!"}
+
+
+@app.post("/make_calculation")
+def operate(input:User_input):
+    result = calculate(input.operation, input.x, input.y)
+    return result
+
+
+# Start web server
+# uvicorn create_api:app --reload
